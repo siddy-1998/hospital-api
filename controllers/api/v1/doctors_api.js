0ce7fcdf7b4837390bb3a6353bd4036f6aa8a234
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-08-17 21:57:05 +0530

    make doctor's login and register controller along with routes

---
+++ controllers/api/v1/doctors_api.js
@@ -0,0 +1,66 @@
+const Doctor =  require('../../../models/doctor');
+const jwt = require('jsonwebtoken');
+
+//for registering doctor
+module.exports.register = async function (req, res) {
+    console.log(req.body);
+    if (req.body == undefined || req.body.username == undefined || req.body.username.length == 0 || !req.body.password) {
+        res.status(200).json({
+            "message": "Please set valid username and password"
+        });
+        return;
+    }
+   
+    try {
+        let doctor = await Doctor.findOne({"username": req.body.username });
+
+        if (doctor) {
+            res.status(200).json({
+                "message": "This username already exists, try something new"
+            });
+            return;
+        } else {
+            
+            let new_doctor = await Doctor.create(req.body);
+            if (new_doctor) {
+                res.status(200).json({
+                    "Message": "New Doctor registered"
+                });
+                return;
+            }
+        }
+    } catch (err) {
+        if (err) {
+            return res.status(500).json({
+                "message": "Internal Server Error"
+            });
+        }
+    }
+
+}
+
+//for signing in the doctor
+module.exports.login = async function (req, res) {
+    try {
+            let doctor = await Doctor.findOne({"username": req.body.username});
+
+            if (doctor.password != req.body.password) 
+            {
+                return res.status(422).json({
+                    "message": "Invalid username or password"
+                    });
+                } else {
+                    res.status(200).json({
+                        "message": "Signed in Successfully",
+                        "token": jwt.sign(doctor.toJSON(), 'hospitalAPI', { expiresIn: 1000000 })
+                });
+            }
+        }
+        catch (err) {
+            if (err) {
+                return res.status(500).json({
+                "message": "Internal Server Error"
+            });
+        }
+    }
+}
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/doctors',require('./doctors'));
+
 module.exports=router;
